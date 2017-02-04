ee7cd62388ff500dfa257b159dda76dd9d193364
Henry Troutman <henguin1001@gmail.com>
2017-02-04 00:10:29 -0500

    added some tests

--- test/twigtex.js
+++ test/twigtex.js
@@ -3,6 +3,7 @@ var assert = require('assert'),
   mock = require('mock-fs'),
   twigtex = require('../lib/twigtex.js');
 
+// Setup testing file syctem
 mock({
   'single':{
     'file.twig.tex': '{{str}}',
@@ -13,15 +14,16 @@ mock({
   },
   'complex':{
     'file1.twig.tex':'{% block head %}0{% endblock%}',
-    'file2.twig.tex':'{% block head %}{{str}}{% endblock%}',
+    'file2.twig.tex':'{% extends "file1.twig.tex" %}{% block head %}{{str}}{% endblock %}',
   }
 });
 after(function (done) {
   mock.restore();
+  done();
 });
 
-describe('Twigtex', function() {
-  describe('#()', function() {
+describe('twigtex', function() {
+  describe('#', function() {
     it('should compile a single file', function(done) {
       twigtex('single/file.twig.tex', function(err, res){
         assert.equal(res, "teststring");
@@ -30,13 +32,27 @@ describe('Twigtex', function() {
         str:"teststring"
       }});
     });
-    it('should compile a multiple files', function(done) {
-      twigtex(['single/file1.twig.tex', 'single/file2.twig.tex'], function(err, res){
-        assert.equal(res, ["teststring", "teststring"]);
+    it('should compile multiple files', function(done) {
+      twigtex(['multi/file1.twig.tex', 'multi/file2.twig.tex'], function(err, res){
+        assert.deepEqual(res, ["teststring", "teststring"]);
         done(err);
       },{ globals:{
         str:"teststring"
       }});
+    });
+    it('should compile imported file chain', function(done) {
+      twigtex('complex/file2.twig.tex', function(err, res){
+        assert.equal(res, 'teststring');
+        done(err);
+      },{ globals:{
+        str:"teststring"
+      }});
+    });
+    it('should throw and error when file doesnt exist', function(done) {
+      twigtex('single/fake.twig.tex', function(err, res){
+        assert(!!err);
+        done();
+      });
     });
   });
 });
